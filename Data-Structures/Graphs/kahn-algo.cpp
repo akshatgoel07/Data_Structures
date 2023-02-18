2755ea155f65894ecc65ecc8d57c7a7fe1b3a5a6
Akshat Goel <49344362+akshato7@users.noreply.github.com>
2023-02-18 13:36:22 +0530

    Create kahn-algo.cpp

---
+++ Data-Structures/Graphs/kahn-algo.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+	public:
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    int indegree[V] = {0};
+	    for(int i =0; i<V; i++){
+	        for(auto it : adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int>q;
+	    vector<int>topo;
+	    
+	    for(int i =0; i<V;i++){
+	        if(indegree[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        topo.push_back(node);
+	        
+	        for(auto it : adj[node]){
+	            indegree[it]--;
+	            if(indegree[it]==0) q.push(it);
+	        }
+	    }
+	    return topo;
+	}
+};
